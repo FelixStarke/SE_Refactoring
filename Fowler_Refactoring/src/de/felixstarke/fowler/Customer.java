972918640f542d6d94b5fdb27999983bcbeaaab2
FelixStarke <starke.felix@t-online.de>
2018-05-04 20:57:01 +0200

    Removed unnecessary method amountFor, this is still part of the previous refactoring; Code Smell: Object-Orientation Abuser.

--- Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
+++ Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
@@ -29,7 +29,7 @@ class Customer {
             double thisAmount;
             Rental each = (Rental) enum_rentals.nextElement();
             //determine amounts for each line
-            thisAmount = amountFor(each);
+            thisAmount = each.getCharge();
             // add frequent renter points
             frequentRenterPoints ++;
             // add bonus for a two day new release rental
@@ -44,9 +44,4 @@ class Customer {
         result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
         return result;
     }
-
-    private double amountFor(Rental rental) {
-        return rental.getCharge();
-    }
-
 }

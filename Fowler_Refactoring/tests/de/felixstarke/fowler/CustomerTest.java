da1d1a8edc571cc7c434b8798d86210d9ea877d6
FelixStarke <starke.felix@t-online.de>
2018-05-04 15:48:44 +0200

    Added more tests for Customer.

--- Fowler_Refactoring/tests/de/felixstarke/fowler/CustomerTest.java
+++ Fowler_Refactoring/tests/de/felixstarke/fowler/CustomerTest.java
@@ -13,7 +13,7 @@ class CustomerTest {
     }
 
     @Test
-    void addRental() {
+    void addRental1() {
         Movie movie = new Movie("Back To The Future", Movie.REGULAR);
         Rental rental = new Rental(movie, 10);
 
@@ -25,6 +25,52 @@ class CustomerTest {
         expected.append("\tBack To The Future\t\t" + rental.getDaysRented() + "\t14.0\n");
         expected.append("Amount owed is 14.0\n");
         expected.append("You earned 1 frequent renter points");
+
+        Assert.assertTrue(customer.statement().compareTo(expected.toString()) == 0);
+    }
+
+    @Test
+    void addRental2() {
+        Movie movie1 = new Movie("Back To The Future", Movie.REGULAR);
+        Movie movie2 = new Movie("Back To The Future III", Movie.NEW_RELEASE);
+        Rental rental1 = new Rental(movie1, 10);
+        Rental rental2 = new Rental(movie2, 5);
+
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+
+        StringBuilder expected = new StringBuilder();
+        expected.append("Rental Record for " + customer.getName() + "\n");
+        expected.append("\tTitle\t\tDays\tAmount\n");
+        expected.append("\tBack To The Future\t\t" + rental1.getDaysRented() + "\t14.0\n");
+        expected.append("\tBack To The Future III\t\t" + rental2.getDaysRented() + "\t15.0\n");
+        expected.append("Amount owed is 29.0\n");
+        expected.append("You earned 3 frequent renter points");
+
+        Assert.assertTrue(customer.statement().compareTo(expected.toString()) == 0);
+    }
+
+    @Test
+    void addRental3() {
+        Movie movie1 = new Movie("Back To The Future", Movie.REGULAR);
+        Movie movie2 = new Movie("Back To The Future II", Movie.CHILDRENS);
+        Movie movie3 = new Movie("Back To The Future III", Movie.NEW_RELEASE);
+        Rental rental1 = new Rental(movie1, 10);
+        Rental rental2 = new Rental(movie2, 5);
+        Rental rental3 = new Rental(movie3, 4);
+
+        customer.addRental(rental1);
+        customer.addRental(rental2);
+        customer.addRental(rental3);
+
+        StringBuilder expected = new StringBuilder();
+        expected.append("Rental Record for " + customer.getName() + "\n");
+        expected.append("\tTitle\t\tDays\tAmount\n");
+        expected.append("\tBack To The Future\t\t" + rental1.getDaysRented() + "\t14.0\n");
+        expected.append("\tBack To The Future II\t\t" + rental2.getDaysRented() + "\t4.5\n");
+        expected.append("\tBack To The Future III\t\t" + rental3.getDaysRented() + "\t12.0\n");
+        expected.append("Amount owed is 30.5\n");
+        expected.append("You earned 4 frequent renter points");
 
         Assert.assertTrue(customer.statement().compareTo(expected.toString()) == 0);
     }

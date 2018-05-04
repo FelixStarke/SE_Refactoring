c60f5437c37974d20da0311efd2d7fbe91209867
FelixStarke <starke.felix@t-online.de>
2018-05-04 21:19:02 +0200

    Extracted temporary variable frequentRenterPoints; Code Smell: Object-Orientation Abuser - Temporary Field, Refactoring technique: Replace Temp with Query.

--- Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
+++ Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
@@ -19,23 +19,19 @@ class Customer {
     }
 
     String statement() {
-        int frequentRenterPoints = 0;
-        Enumeration enum_rentals = rentals.elements();	    
+        Enumeration enum_rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
         while (enum_rentals.hasMoreElements()) {
             Rental each = (Rental) enum_rentals.nextElement();
 
-            // add frequent renter points
-            frequentRenterPoints += each.getFrequentRenterPoints();
-
             //show figures for this rental
             result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
-        result += "You earned " + String.valueOf(frequentRenterPoints) + " frequent renter points";
+        result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
         return result;
     }
 
@@ -45,6 +41,16 @@ class Customer {
         while(_rentals.hasMoreElements()) {
             Rental each = (Rental) _rentals.nextElement();
             result += each.getCharge();
+        }
+        return result;
+    }
+
+    private int getTotalFrequentRenterPoints() {
+        int result = 0;
+        Enumeration _rentals = rentals.elements();
+        while(_rentals.hasMoreElements()) {
+            Rental each = (Rental) _rentals.nextElement();
+            result += each.getFrequentRenterPoints();
         }
         return result;
     }

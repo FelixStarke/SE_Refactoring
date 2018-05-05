560e2cdd4ebf67592de273da3509be611ea7ac07
FelixStarke <starke.felix@t-online.de>
2018-05-05 16:46:55 +0200

    Added htmlStatement.

--- Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
+++ Fowler_Refactoring/src/de/felixstarke/fowler/Customer.java
@@ -19,19 +19,38 @@ class Customer {
     }
 
     String statement() {
-        Enumeration enum_rentals = rentals.elements();
+        Enumeration _rentals = rentals.elements();
         String result = "Rental Record for " + this.getName() + "\n";
         result += "\t" + "Title" + "\t" + "\t" + "Days" + "\t" + "Amount" + "\n";
 
-        while (enum_rentals.hasMoreElements()) {
-            Rental each = (Rental) enum_rentals.nextElement();
+        while (_rentals.hasMoreElements()) {
+            Rental each = (Rental) _rentals.nextElement();
 
             //show figures for this rental
-            result += "\t" + each.getMovie().getTitle()+ "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
+            result += "\t" + each.getMovie().getTitle() + "\t" + "\t" + each.getDaysRented() + "\t" + String.valueOf(each.getCharge()) + "\n";
         }
+
         //add footer lines
         result += "Amount owed is " + String.valueOf(getTotalCharge()) + "\n";
         result += "You earned " + String.valueOf(getTotalFrequentRenterPoints()) + " frequent renter points";
+        return result;
+    }
+
+    String htmlStatement() {
+        Enumeration _rentals = rentals.elements();
+        String result = "<h1>Rentals for <em>" + getName() + "</em></h1><p>\n";
+
+        while(_rentals.hasMoreElements()) {
+            Rental each = (Rental) _rentals.nextElement();
+
+            //show figures for this rental
+            result += each.getMovie().getTitle() + ": " + String.valueOf(each.getCharge()) + "<br />\n";
+        }
+
+        // add footer lines
+        result += "</p><p>You owe <em>" + String.valueOf(getTotalCharge()) + "</em></p>\n";
+        result += "On this rental you earned <em>" + String.valueOf(getTotalFrequentRenterPoints()) + "</em> frequent renter points</p>";
+
         return result;
     }
 
